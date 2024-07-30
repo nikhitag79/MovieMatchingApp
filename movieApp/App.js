97fbdc54173f89f9b55b6fd9fd24b0a0aeaf0186
nikhitag79 <nguntu@gmail.com>
2024-07-29 22:38:39 -0400

    card is moving

--- movieApp/App.js
+++ movieApp/App.js
@@ -1,13 +1,30 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, Image, ImageBackground, View, StyleSheet} from 'react-native';
+import { Text, Image, ImageBackground, View, StyleSheet, Pressable} from 'react-native';
 import Card from './src/components/Card';
 import users from './assets/data/users'
+
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, useAnimatedGestureHandler } from 'react-native-reanimated'
 const App = () => {
+
+  const sharedValue = useSharedValue(1);
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [{
+      translateX: sharedValue.value *500-250, 
+    }]
+    
+
+  }));
+
   return (
     <View style = {styles.pageContainer}>
+      <Animated.View style= {[styles.animatedCard, cardStyle]}> 
       <Card user = {users[2]}/>
-
-      
+      </Animated.View> 
+      <Pressable onPress={()=> (sharedValue.value = withSpring(Math.random()))}>
+        
+        <Text>Change Value</Text></Pressable>
   </View>
 
   );
@@ -18,6 +35,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
      alignItems: 'center', 
      flex:1
+  }, 
+  animatedCard: {
+    width: '100%', 
+    justifyContent: 'center',
+    alignItems: 'center', 
   }
 
 
