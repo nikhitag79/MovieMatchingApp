694093cbdb1b7a12827afc69154275e9d958282b
nikhitag79 <nguntu@gmail.com>
2024-07-30 22:12:54 -0400

    card moves

--- movieApp/App.js
+++ movieApp/App.js
@@ -1,48 +1,58 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, Image, ImageBackground, View, StyleSheet, Pressable} from 'react-native';
+import { Text, Image, ImageBackground, View, StyleSheet, Pressable } from 'react-native';
 import Card from './src/components/Card';
-import users from './assets/data/users'
+import users from './assets/data/users';
 
-import Animated, { useSharedValue, useAnimatedStyle, withSpring, useAnimatedGestureHandler } from 'react-native-reanimated'
+import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import { GestureDetector, Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
+
 const App = () => {
-
   const sharedValue = useSharedValue(1);
 
   const cardStyle = useAnimatedStyle(() => ({
     transform: [{
-      translateX: sharedValue.value *500-250, 
+      translateX: sharedValue.value * 500 - 250,
     }]
-    
-
   }));
 
-  return (
-    <View style = {styles.pageContainer}>
-      <Animated.View style= {[styles.animatedCard, cardStyle]}> 
-      <Card user = {users[2]}/>
-      </Animated.View> 
-      <Pressable onPress={()=> (sharedValue.value = withSpring(Math.random()))}>
-        
-        <Text>Change Value</Text></Pressable>
-  </View>
+  const panGesture = Gesture.Pan()
+    .onStart(() => {
+      console.warn("Touch Start");
+    })
+    .onUpdate((event) => {
+      console.log("Touch x: ", event.translationX);
+      sharedValue.value = withSpring(event.translationX / 500 + 0.5);
+    })
+    .onEnd(() => {
+      console.warn("Touch end");
+      sharedValue.value = withSpring(1); // Reset to original position
+    });
 
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={styles.pageContainer}>
+        <GestureDetector gesture={panGesture}>
+          <Animated.View style={[styles.animatedCard, cardStyle]}>
+            <Card user={users[2]} />
+          </Animated.View>
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
   );
-}; 
+};
 
 const styles = StyleSheet.create({
   pageContainer: {
     justifyContent: 'center',
-     alignItems: 'center', 
-     flex:1
-  }, 
+    alignItems: 'center',
+    flex: 1
+  },
   animatedCard: {
-    width: '100%', 
+    width: '100%',
     justifyContent: 'center',
-    alignItems: 'center', 
+    alignItems: 'center',
   }
-
-
-})
+});
 
 export default App;
