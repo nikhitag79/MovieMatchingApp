7f4dbc2b01a1b2cecb269be6ca7da188b7fd016d
nikhitag79 <nguntu@gmail.com>
2024-09-01 19:43:17 -0400

    finished authentication

--- movieApp/App.js
+++ movieApp/App.js
@@ -1,23 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, useWindowDimensions, Image, SafeAreaView, Pressable } from 'react-native';
-import Card from './src/components/Card';
-import users from './assets/data/users';
-import Like from './assets/yes.jpg';
-import Dislike from './assets/no.webp';
-import AnimatedStack from './src/components/AnimatedStack';
-
-import Animated, { useSharedValue, useAnimatedStyle, useDerivedValue, interpolate, withSpring, runOnJS} from 'react-native-reanimated';
-import { GestureHandlerRootView, PanGestureHandler } from 'react-native-gesture-handler';
+import { View, StyleSheet, SafeAreaView, Pressable } from 'react-native';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import HomeScreen from './src/screens/HomeScreens';
- 
 
-
-const ROTATION = 60;
-const SWIPE_VELOCITY = 600;
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native'; // Import the withAuthenticator HOC
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 
 const App = () => {
   const [activeScreen, setActiveScreen] = useState("HOME");
@@ -25,42 +17,35 @@ const App = () => {
   const activeColor = '#f76c6b';
 
   return (
-    <GestureHandlerRootView style={ styles.root }>
-      <SafeAreaView style = {{flex: 1}} > 
+    <SafeAreaView style={styles.root}>
       <View style={styles.pageContainer}>
-        <View style = {styles.topNavigation}>
-          <Pressable onPress = {() => setActiveScreen('HOME')}>
-            <Fontisto name = "tinder" size={30} color={activeScreen === 'HOME' ? activeColor : color}/>
-         </Pressable>
-         <MaterialCommunityIcons name="star-four-points" size ={30} color = {color}/>
-         <Pressable  onPress = {() => setActiveScreen('CHAT')}>
-          <Ionicons name = 'chatbubbles' size = {30} color = {activeScreen === 'CHAT' ? activeColor : color}/>
-
-         </Pressable>
-            <FontAwesome name = "user" size = {30} color = {color}/>
+        <View style={styles.topNavigation}>
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Fontisto name="tinder" size={30} color={activeScreen === 'HOME' ? activeColor : color} />
+          </Pressable>
+          <MaterialCommunityIcons name="star-four-points" size={30} color={color} />
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons name='chatbubbles' size={30} color={activeScreen === 'CHAT' ? activeColor : color} />
+          </Pressable>
+          <FontAwesome name="user" size={30} color={color} />
         </View>
         
-      { activeScreen === 'HOME' && <HomeScreen/> } 
+        {activeScreen === 'HOME' && <HomeScreen />}
       </View>
-      </SafeAreaView>
-     
-    </GestureHandlerRootView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-
     flex: 1,
   },
-  topNavigation:{
-
+  topNavigation: {
     flexDirection: 'row',
     justifyContent: 'space-around',
     width: '100%',
     padding: 10,
   },
-
   pageContainer: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -68,4 +53,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default withAuthenticator(App); // Wrap your App with the withAuthenticator HOC
